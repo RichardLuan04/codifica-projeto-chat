023a7dc1aabf10d598a9bfe22393b327a4bef032
Richard Luan <richardluan084@gmail.com>
2022-10-20 21:02:12 -0300

    Ajustando function

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 var usuario1 = []
 var usuario2 = []
 
-function Mostrar_mensagem_um() {
-    var mensagem = document.getElementById("mensagemUsuario1").value
+function Mostrar_mensagem(name) {
+    var mensagem = document.getElementById(name).value
     usuario1.push(mensagem)
+
     var li = document.createElement("li")
-    var lista = document.getElementById("listaDeMensagens")
+    li.className = name
     li.innerText = mensagem
-    lista.append(li)
-    mensagem.value = ""
-}
 
-function Mostrar_mensagem_dois() {
-    var mensagem = document.getElementById("mensagemUsuario2").value
-    usuario2.push(mensagem)
-     var li = document.createElement("li")
     var lista = document.getElementById("listaDeMensagens")
-    li.innerText = mensagem
     lista.append(li)
-    mensagem.value = ""
+    
+    document.getElementById(name).value = ""
 }
-
