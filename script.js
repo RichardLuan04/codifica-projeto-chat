4db91eb926ebbc061bca68bf42650ec18db352a0
Richard Luan <richardluan084@gmail.com>
2022-10-24 22:02:06 -0300

    Atualizando localStorage para manter as mensagens

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-var usuario1 = []
-var usuario2 = []
+var chat = []
+var mensagensSalvas = localStorage.getItem("Chat")
+ 
+if (mensagensSalvas !== null){
+    chat = JSON.parse(mensagensSalvas)
+
+    var lista = document.getElementById("listaDeMensagens")
+
+    for (var mensagem of chat) {
+        var li = document.createElement("li")
+        li.innerText = mensagem.texto
+                
+        if (mensagem.usuario === "usuario1"){
+            li.className = 'mensagemUsuario1'
+        } else {
+            li.className = 'mensagemUsuario2'
+        }
+        lista.append(li)
+    }
+}
 
 function Mostrar_mensagem(name,user) {
-    var mensagem = document.getElementById(name).value
-    user.push(mensagem)
+    var campoTexto = document.getElementById(name)
 
     var li = document.createElement("li")
     li.className = name
-    li.innerText = mensagem
+    li.innerText = campoTexto.value
 
-    var lista = document.getElementById("listaDeMensagens")
-    lista.append(li)
+    document.getElementById("listaDeMensagens").append(li)
     
-    document.getElementById(name).value = ""
+    var informacoes_mensagem = {
+        texto: campoTexto.value,
+        usuario: user
+    }
+
+    chat.push(informacoes_mensagem)
+
+    campoTexto.value = ""
+
+    var chatJSON = JSON.stringify(chat)
+    localStorage.setItem('Chat', chatJSON)
 }
